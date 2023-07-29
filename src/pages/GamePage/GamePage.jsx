b46d6aafc099bd53f3ff4ef2b9f418a5769aa9f5
Pamela Arteaga Campos <pamelaarteaga@MacBook-Pro-de-Pamela.local>
2023-07-29 16:18:39 -0400

    Add duplicates images function

--- src/pages/GamePage/GamePage.jsx
+++ src/pages/GamePage/GamePage.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getImagesList } from "../../store/images/imagesThunks";
+import Spinner from "../../ui/components/Spinner/Spinner";
 
 const GamePage = () => {
   const dispatch = useDispatch();
@@ -11,21 +12,40 @@ const GamePage = () => {
 
   const { userName } = useSelector((state) => state.auth);
   const { images } = useSelector((state) => state.images);
-  console.log(images);
+
+  //shuffle images order
+  const shuffle = (arr) => {
+    const newArr = [...arr];
+    return newArr.sort(() => Math.random() - 0.5);
+  };
+
+  //duplicates the images and returns a new list
+  const shuffleImages = (list) => {
+    const newList = shuffle(list).slice(0, 6);
+    const duplicates = [...newList, ...newList];
+
+    return shuffle(duplicates);
+  };
 
   return (
     <div>
       <p>Hello {userName}!</p>
       <div className="flex justify-between flex-wrap">
-        {images.map((image, i) => (
-          <div key={i} className="w-3/12 h-96 overflow-hidden">
-            <img
-              className="object-contain"
-              src={image.fields.image.url}
-              alt={image.meta.name}
-            />
-          </div>
-        ))}
+        {images.length === 0 ? (
+          <Spinner />
+        ) : (
+          <>
+            {shuffleImages(images).map((image, i) => (
+              <div key={i} className="w-3/12 h-96 overflow-hidden">
+                <img
+                  className="object-contain"
+                  src={image.fields.image.url}
+                  alt={image.meta.name}
+                />
+              </div>
+            ))}
+          </>
+        )}
       </div>
     </div>
   );
